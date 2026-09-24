484d5473e36dcc9e7b3fc3e4f65396a0b311c03d
Bhargav Ahire <bhargavahire910@gmail.com>
2026-09-24 14:15:51 +0530

    Add missing Switch component

---
+++ src/components/ui/switch.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import * as React from "react";
+
+interface SwitchProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  checked: boolean;
+  onCheckedChange: (checked: boolean) => void;
+}
+
+export function Switch({ checked, onCheckedChange, disabled, className = "", ...props }: SwitchProps) {
+  return (
+    <button
+      type="button"
+      role="switch"
+      aria-checked={checked}
+      disabled={disabled}
+      onClick={() => onCheckedChange(!checked)}
+      className={`
+        relative inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full 
+        border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2
+        ${checked ? 'bg-primary' : 'bg-gray-300'}
+        ${disabled ? 'opacity-50 cursor-not-allowed' : ''}
+        ${className}
+      `}
+      {...props}
+    >
+      <span
+        aria-hidden="true"
+        className={`
+          pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out
+          ${checked ? 'translate-x-5' : 'translate-x-0'}
+        `}
+      />
+    </button>
+  );
+}
